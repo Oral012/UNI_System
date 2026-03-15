3f05f4de10b2df92939415127996d511d30d519f
unknown <norakvannada.din@student.cadt.edu.kh>
2026-03-15 19:43:01 +0700

    added add/remove stu,crs|view all crs

--- uni_system/src/main/java/uni/system/model/Admin.java
+++ uni_system/src/main/java/uni/system/model/Admin.java
@@ -1,5 +1,7 @@
 package uni.system.model;
 
+import java.util.ArrayList;
+
 public class Admin extends User {
     
     public Admin ( String name, String email, String password, String role, String status ){
@@ -8,6 +10,27 @@ public class Admin extends User {
     
     public String getRole ( ) {
         return "Admin";
+    }
+
+    public void addCourse(ArrayList<Course> coursesTaught, Course course) {
+        coursesTaught.add(course);
+    }
+    public void addStudent(ArrayList<Student> students, Student student) {
+        students.add(student);
+    }
+    public void removeCourse(ArrayList<Course> coursesTaught, String courseId){
+        for (Course c : coursesTaught) {
+            if (c.getCourseId().equals(courseId)) {
+                coursesTaught.remove(c);
+                break;
+            }
+        }
+    }  
+
+    public void viewAllCourses(ArrayList<Course> coursesTaught) {
+        for (Course c : coursesTaught) {
+            System.out.println(c);
+        }
     }
     
 }
